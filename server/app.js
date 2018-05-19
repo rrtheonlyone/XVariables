b46d1538ac5a7e958c078d329102f6eb7401e74d
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-05-19 11:39:22 +0800

    Updated mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
+//dependancies
 const express = require('express')
 const app = express()
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
