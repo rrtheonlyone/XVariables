10f2f5376bfaaa1c8afd0021cdcf2630ece8ed99
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-05-19 19:00:42 +0800

    Added frontend for webcam

---
+++ client/src/ReactPlayer.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+
+import { propTypes, defaultProps, DEPRECATED_CONFIG_PROPS } from './props'
+import { getConfig, omit, isEqual } from './utils'
+import players from './players'
+import Player from './Player'
+import { FilePlayer } from './players/FilePlayer'
+import renderPreloadPlayers from './preload'
+
+const SUPPORTED_PROPS = Object.keys(propTypes)
+
+let customPlayers = []
+
+export default class ReactPlayer extends Component {
+  static addCustomPlayer = player => {
+    customPlayers.push(player)
+  }
+  static removeCustomPlayers = () => {
+    customPlayers = []
+  }
+  static displayName = 'ReactPlayer'
+  static propTypes = propTypes
+  static defaultProps = defaultProps
+  static canPlay = url => {
+    for (let Player of [ ...customPlayers, ...players ]) {
+      if (Player.canPlay(url)) {
+        return true
+      }
+    }
+    return false
+  }
+  config = getConfig(this.props, defaultProps, true)
+  componentDidMount () {
+    if (this.props.progressFrequency) {
+      const message = 'ReactPlayer: %cprogressFrequency%c is deprecated, please use %cprogressInterval%c instead'
+      console.warn(message, 'font-weight: bold', '', 'font-weight: bold', '')
+    }
+  }
+  shouldComponentUpdate (nextProps) {
+    return !isEqual(this.props, nextProps)
+  }
+  componentWillUpdate (nextProps) {
+    this.config = getConfig(nextProps, defaultProps)
+  }
+  getDuration = () => {
+    if (!this.player) return null
+    return this.player.getDuration()
+  }
+  getCurrentTime = () => {
+    if (!this.player) return null
+    return this.player.getCurrentTime()
+  }
+  getSecondsLoaded = () => {
+    if (!this.player) return null
+    return this.player.getSecondsLoaded()
+  }
+  getInternalPlayer = (key = 'player') => {
+    if (!this.player) return null
+    return this.player.getInternalPlayer(key)
+  }
+  seekTo = fraction => {
+    if (!this.player) return null
+    this.player.seekTo(fraction)
+  }
+  getActivePlayer (url) {
+    for (let Player of [ ...customPlayers, ...players ]) {
+      if (Player.canPlay(url)) {
+        return Player
+      }
+    }
+    // Fall back to FilePlayer if nothing else can play the URL
+    return FilePlayer
+  }
+  wrapperRef = wrapper => {
+    this.wrapper = wrapper
+  }
+  activePlayerRef = player => {
+    this.player = player
+  }
+  renderActivePlayer (url) {
+    if (!url) return null
+    const activePlayer = this.getActivePlayer(url)
+    return (
+      <Player
+        {...this.props}
+        key={activePlayer.displayName}
+        ref={this.activePlayerRef}
+        config={this.config}
+        activePlayer={activePlayer}
+      />
+    )
+  }
+  sortPlayers (a, b) {
+    // Retain player order to prevent weird iframe behaviour when switching players
+    if (a && b) {
+      return a.key < b.key ? -1 : 1
+    }
+    return 0
+  }
+  render () {
+    const { url, style, width, height, wrapper: Wrapper } = this.props
+    const otherProps = omit(this.props, SUPPORTED_PROPS, DEPRECATED_CONFIG_PROPS)
+    const activePlayer = this.renderActivePlayer(url)
+    const preloadPlayers = renderPreloadPlayers(url, this.config)
+    const players = [ activePlayer, ...preloadPlayers ].sort(this.sortPlayers)
+    return (
+      <Wrapper ref={this.wrapperRef} style={{ ...style, width, height }} {...otherProps}>
+        {players}
+      </Wrapper>
+    )
+  }
+}
---
+++ client/src/players/FilePlayer.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react'
+
+import { getSDK } from '../utils'
+import createSinglePlayer from '../singlePlayer'
+
+const IOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+const AUDIO_EXTENSIONS = /\.(m4a|mp4a|mpga|mp2|mp2a|mp3|m2a|m3a|wav|weba|aac|oga|spx)($|\?)/i
+const VIDEO_EXTENSIONS = /\.(mp4|og[gv]|webm|mov|m4v)($|\?)/i
+const HLS_EXTENSIONS = /\.(m3u8)($|\?)/i
+const HLS_SDK_URL = 'https://cdnjs.cloudflare.com/ajax/libs/hls.js/0.8.9/hls.min.js'
+const HLS_GLOBAL = 'Hls'
+const DASH_EXTENSIONS = /\.(mpd)($|\?)/i
+const DASH_SDK_URL = 'https://cdnjs.cloudflare.com/ajax/libs/dashjs/2.6.5/dash.all.min.js'
+const DASH_GLOBAL = 'dashjs'
+const MATCH_DROPBOX_URL = /www\.dropbox\.com\/.+/
+
+function canPlay (url) {
+  if (url instanceof Array) {
+    for (let item of url) {
+      if (typeof item === 'string' && canPlay(item)) {
+        return true
+      }
+      if (canPlay(item.src)) {
+        return true
+      }
+    }
+    return false
+  }
+  if (url instanceof window.MediaStream) {
+    return true
+  }
+  return (
+    AUDIO_EXTENSIONS.test(url) ||
+    VIDEO_EXTENSIONS.test(url) ||
+    HLS_EXTENSIONS.test(url) ||
+    DASH_EXTENSIONS.test(url)
+  )
+}
+
+export class FilePlayer extends Component {
+  static displayName = 'FilePlayer'
+  static canPlay = canPlay
+
+  componentDidMount () {
+    this.addListeners()
+    if (IOS) {
+      this.player.load()
+    }
+  }
+  componentWillReceiveProps (nextProps) {
+    if (this.shouldUseAudio(this.props) !== this.shouldUseAudio(nextProps)) {
+      this.removeListeners()
+    }
+  }
+  componentDidUpdate (prevProps) {
+    if (this.shouldUseAudio(this.props) !== this.shouldUseAudio(prevProps)) {
+      this.addListeners()
+    }
+  }
+  componentWillUnmount () {
+    this.removeListeners()
+  }
+  addListeners () {
+    const { onReady, onPlay, onPause, onEnded, onError, playsinline } = this.props
+    this.player.addEventListener('canplay', onReady)
+    this.player.addEventListener('play', onPlay)
+    this.player.addEventListener('pause', onPause)
+    this.player.addEventListener('seeked', this.onSeek)
+    this.player.addEventListener('ended', onEnded)
+    this.player.addEventListener('error', onError)
+    if (playsinline) {
+      this.player.setAttribute('playsinline', '')
+      this.player.setAttribute('webkit-playsinline', '')
+    }
+  }
+  removeListeners () {
+    const { onReady, onPlay, onPause, onEnded, onError } = this.props
+    this.player.removeEventListener('canplay', onReady)
+    this.player.removeEventListener('play', onPlay)
+    this.player.removeEventListener('pause', onPause)
+    this.player.removeEventListener('seeked', this.onSeek)
+    this.player.removeEventListener('ended', onEnded)
+    this.player.removeEventListener('error', onError)
+  }
+  onSeek = e => {
+    this.props.onSeek(e.target.currentTime)
+  }
+  shouldUseAudio (props) {
+    if (props.config.file.forceVideo) {
+      return false
+    }
+    if (props.config.file.attributes.poster) {
+      return false // Use <video> so that poster is shown
+    }
+    return AUDIO_EXTENSIONS.test(props.url) || props.config.file.forceAudio
+  }
+  shouldUseHLS (url) {
+    return (HLS_EXTENSIONS.test(url) && !IOS) || this.props.config.file.forceHLS
+  }
+  shouldUseDASH (url) {
+    return DASH_EXTENSIONS.test(url) || this.props.config.file.forceDASH
+  }
+  load (url) {
+    if (this.shouldUseHLS(url)) {
+      getSDK(HLS_SDK_URL, HLS_GLOBAL).then(Hls => {
+        this.hls = new Hls(this.props.config.file.hlsOptions)
+        this.hls.on(Hls.Events.ERROR, (e, data) => {
+          this.props.onError(e, data, this.hls, Hls)
+        })
+        this.hls.loadSource(url)
+        this.hls.attachMedia(this.player)
+      })
+    }
+    if (this.shouldUseDASH(url)) {
+      getSDK(DASH_SDK_URL, DASH_GLOBAL).then(dashjs => {
+        this.dash = dashjs.MediaPlayer().create()
+        this.dash.initialize(this.player, url, this.props.playing)
+        this.dash.getDebug().setLogToBrowserConsole(false)
+      })
+    }
+    if (url instanceof window.MediaStream) {
+      try {
+        this.player.srcObject = url
+      } catch (e) {
+        this.player.src = window.URL.createObjectURL(url)
+      }
+    }
+  }
+  play () {
+    const promise = this.player.play()
+    if (promise) {
+      promise.catch(this.props.onError)
+    }
+  }
+  pause () {
+    this.player.pause()
+  }
+  stop () {
+    this.player.removeAttribute('src')
+    if (this.hls) {
+      this.hls.destroy()
+    }
+    if (this.dash) {
+      this.dash.reset()
+    }
+  }
+  seekTo (seconds) {
+    this.player.currentTime = seconds
+  }
+  setVolume (fraction) {
+    this.player.volume = fraction
+  }
+  mute = () => {
+    this.player.muted = true
+  }
+  unmute = () => {
+    this.player.muted = false
+  }
+  setPlaybackRate (rate) {
+    this.player.playbackRate = rate
+  }
+  getDuration () {
+    return this.player.duration
+  }
+  getCurrentTime () {
+    return this.player.currentTime
+  }
+  getSecondsLoaded () {
+    const { buffered } = this.player
+    if (buffered.length === 0) {
+      return 0
+    }
+    const end = buffered.end(buffered.length - 1)
+    const duration = this.getDuration()
+    if (end > duration) {
+      return duration
+    }
+    return end
+  }
+  getSource (url) {
+    const useHLS = this.shouldUseHLS(url)
+    const useDASH = this.shouldUseDASH(url)
+    if (url instanceof Array || url instanceof window.MediaStream || useHLS || useDASH) {
+      return undefined
+    }
+    if (MATCH_DROPBOX_URL.test(url)) {
+      return url.replace('www.dropbox.com', 'dl.dropboxusercontent.com')
+    }
+    return url
+  }
+  renderSourceElement = (source, index) => {
+    if (typeof source === 'string') {
+      return <source key={index} src={source} />
+    }
+    return <source key={index} {...source} />
+  }
+  renderTrack = (track, index) => {
+    return <track key={index} {...track} />
+  }
+  ref = player => {
+    this.player = player
+  }
+  render () {
+    const { url, playing, loop, controls, muted, config, width, height } = this.props
+    const useAudio = this.shouldUseAudio(this.props)
+    const Element = useAudio ? 'audio' : 'video'
+    const style = {
+      width: width === 'auto' ? width : '100%',
+      height: height === 'auto' ? height : '100%'
+    }
+    return (
+      <Element
+        ref={this.ref}
+        src={this.getSource(url)}
+        style={style}
+        preload='auto'
+        autoPlay={playing}
+        controls={controls}
+        muted={muted}
+        loop={loop}
+        {...config.file.attributes}>
+        {url instanceof Array &&
+          url.map(this.renderSourceElement)
+        }
+        {config.file.tracks.map(this.renderTrack)}
+      </Element>
+    )
+  }
+}
+
+export default createSinglePlayer(FilePlayer)
