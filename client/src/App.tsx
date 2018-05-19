ae490885fb417a2045bb92c2a1a10c59d9f4028a
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-05-19 13:25:30 +0800

    Added React Player for watching videos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import ReactPlayer from 'react-player'
 import NavigationBar from './NavBar'
 
+ 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <NavigationBar title="XVariables" />
+        <ReactPlayer url='https://www.youtube.com/watch?v=QwievZ1Tx-8' />
       </div>
     );
   }
