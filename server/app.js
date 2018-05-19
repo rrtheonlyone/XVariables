e2124b17af41f431df31d60099105ae7d35f3f07
ksjagtap <ksjagtap@gmail.com>
2018-05-19 13:54:22 +0800

    semi-working schema done. Need to test first before proceeding

--- server/app.js
+++ server/app.js
@@ -18,6 +18,34 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// mongoose schema go here
+var usersSchema = new Schema({
+	_id: Schema.Types.ObjectID,
+	firstName: String,
+	lastName: String,
+	videos: [{
+		type: Schema.Types.ObjectID,
+		ref: 'Video'
+		// emotions split by timeframe will go here dynamically
+	}]
+});
+
+var videoSchema = new Schema({
+	_id: Schema.Types.ObjectID,
+	name: String,
+	users: [{
+		type: Schema.Types.ObjectID,
+		ref: 'User'
+	}]
+});
+
+
+var Video = mongoose.model('Video', videoSchema);
+var User = mongoose.model('User', userSchema);
+
+app.get('/users', function(req, res){
+	
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
